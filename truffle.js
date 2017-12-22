4b93999141d37985dcf999d287db0757178c9d65
EduardVavrinchuk <edikvavrinchuk@gmail.com>
2017-12-22 15:40:31 +0200

    initial commit

---
+++ truffle.js
@@ -0,0 +1,15 @@
+module.exports = {
+	networks: {
+	testrpc: {
+	host: "localhost",
+	port: 8545,
+	network_id: "*"
+	},
+	rinkeby: {
+	host: "localhost",
+	port: 8545,
+	network_id: "*",
+	from: "0xFc0640811528b2d20c528a660AFD3888Fe4543C1"
+    }
+   }
+};
